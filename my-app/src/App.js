bb9f9d40205f3bf54c874e734008b7a754524054
nayanendu naskar <nayan19981@gmail.com>
2023-05-17 23:56:14 +0530

    pages added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
-import logo from "./logo.png";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./Home";
+import About from "./About";
+import Product from "./Product";
+import Contact from "./contact";
+import SingleProduct from "./SingleProduct";
+import Cart from "./Cart";
+import WishList from "./WishList";
+import ErrorPage from "./ErrorPage";
+import UserProfile from "./UserProfile";
 
 
 function App() {
@@ -9,6 +16,14 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<WishList />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/userprofile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
