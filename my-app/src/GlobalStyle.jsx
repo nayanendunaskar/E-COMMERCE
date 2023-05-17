7be1cc501371d62bf3f927e4f52601cadc0f269a
nayanendu naskar <nayan19981@gmail.com>
2023-05-18 01:36:39 +0530

    minor changes in styles

--- my-app/src/GlobalStyle.jsx
+++ my-app/src/GlobalStyle.jsx
@@ -1,12 +1,3 @@
-import React from 'react'
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle
-
-const GlobalStyle = () => {
-  return (
-    <div>GlobalStyle</div>
-  )
-}
-
-export default GlobalStyle
+export const GlobalStyle = createGlobalStyle``;
