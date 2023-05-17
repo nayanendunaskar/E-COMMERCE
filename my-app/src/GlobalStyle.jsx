91a281ff8449d4bbfd1712ecc58eee776fb3981e
nayanendu naskar <nayan19981@gmail.com>
2023-05-18 00:49:51 +0530

    style page added

---
+++ my-app/src/GlobalStyle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle
+
+const GlobalStyle = () => {
+  return (
+    <div>GlobalStyle</div>
+  )
+}
+
+export default GlobalStyle
